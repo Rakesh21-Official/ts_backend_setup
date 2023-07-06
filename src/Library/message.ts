b383ab538f369c518226dcf1dfc1c352d7e48fe1
rakesh.official <rakesh21.official@gmail.com>
2023-07-06 14:03:16 +0530

    initial setup

---
+++ src/Library/message.ts
@@ -0,0 +1,17 @@
+enum Messagecode {
+    successful = 'Successful',
+    error = 'Error',
+    register_success = 'Register Successfully',
+    register_error = 'Failed To Add',
+    register_warning = 'Warning Error',
+    address_success = 'User Information Added',
+    vendor_success = 'Register Successfully',
+    user_info = "UserInformation Added Successfully",
+    user_group = "UserGroup Added",
+    user_delete = "User Deleted",
+    vendor_info = "Vendor Added Successfully",
+    admin_add = 'Admin Addes Successfully',
+    admin_delete = 'AdminDetails Removed',
+    vendor_update = 'Vendor Details updated Successfully'
+}
+export default Messagecode
---
+++ src/Library/responsecode.ts
@@ -0,0 +1,8 @@
+export enum HTTPResponseCode {
+    SUCCESS = 200,
+    NO_CONTENT = 204,
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    METHOD_NOT_ALLOWED = 405,
+    INTERNAL_SERVER_ERROR = 500
+}
